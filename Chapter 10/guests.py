581d937068d08d60b43e6cacf8a0771b3a6f1a50
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-03-16 03:56:50 +0000

    something better

--- Chapter 10/guests.py
+++ Chapter 10/guests.py
@@ -1,25 +1,45 @@
 from datetime import datetime as dt
 
-filepath = 'Chapter 10\\text files\\guests.txt'
-names = []
+filepath_users = 'Chapter 10\\text files\\guests.txt'
+filepath_responses = 'Chapter 10\\text files\\responses.txt'
+users = {}
 
-with open(filepath) as file_object:
+class User:
+    """User object with a few basic arguments."""
+
+    def __init__(self, id, name, date):
+
+        self.id = id
+        self.name = name
+        self.date = date
+
+with open(filepath_users) as file_object:
     for line in file_object.readlines():
-        names.append(line.rstrip().split('-'))
+        user_info = line.rstrip().split('-')
+        users[user_info[1]] = (User(user_info[0], user_info[1], user_info[2]))
 
 def AddGuest(name):
     """Adding a guest name to a specific file."""
-    i = str(len(names) + 1)
-    #date = dt.utcnow()
-    #date = dt.strftime('%d%m%Y', date)
-    with open(filepath, 'a') as file_object:
-        file_object.write(i + '-' + name + '\n')
+    i = str(len(users) + 1)
+    date = dt.utcnow()
+    date = date.strftime('%d/%b/%Y')
+    user_info = f'{i}-{name}-{date}'
+    with open(filepath_users, 'a') as file_object:
+        file_object.write(user_info + '\n')
 
-name = input('Name? - ').lower()
+while True:
+    name = input('Name? - ').lower()
+    if name == 'q':
+        print('Quiting...')
+        break
 
-if name not in names:
-    print(f'{name.title()}, you are not in the list... Please come again.')
-    AddGuest(name)
-else:
-    print(f'Last visit: ')
-    print(f'{name.title()}! Welcome back.')
+    if name not in users:
+        print(f'{name.title()}, you are not in the list... Please come again.')
+        AddGuest(name)
+    else:
+        print(f'Last visit of id{users[name].id}: {users[name].date}')
+        print(f'{name.title()}! Welcome back.')
+        cmd = input('Why do  you like programming? - ')
+        with open(filepath_responses, 'a') as file_object:
+            file_object.write(cmd + '\n')
+        print('Response added!')
