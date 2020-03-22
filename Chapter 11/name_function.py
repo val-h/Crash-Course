4d55bdb460e9a86f03261201a71498c630f49621
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-03-22 00:30:17 +0000

    trying out gitignore

--- Chapter 11/name_function.py
+++ Chapter 11/name_function.py
@@ -1,8 +1,12 @@
 #first = input('First name: ')
+#middle = input('Middle name: ')
 #last = input('Last name: ')
 
-def get_formatted_name(first, last):
-    full_name = f'{first} {last}'
+def get_formatted_name(first, last, middle=''):
+    if middle:
+        full_name = f'{first} {middle} {last}'
+    else:
+        full_name = f'{first} {last}'
     return full_name.title()
 
 #print(get_formatted_name(first, last))
--- Chapter 11/test_name_function.py
+++ Chapter 11/test_name_function.py
@@ -5,9 +5,14 @@ class NameTestCase(unittest.TestCase):
     """Tests for name_function.py"""
 
     def test_first_last_name(self):
-        """Do random names like Miskit Fiskit"""
-        formated_name = get_formatted_name('miskit', 'fiskit')
-        self.assertEqual(formated_name, 'Miskit Fiskit')
+        """Do random names like Miskit Fiskit work"""
+        formatted_name = get_formatted_name('miskit', 'fiskit', 'biskit')
+        self.assertEqual(formatted_name, 'Miskit Biskit Fiskit')
+    
+    def test_first_middle_last_name(self):
+        """Do names like Wolfgang Amadeus Mozart work"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 if __name__ == '__main__':
     unittest.main()
