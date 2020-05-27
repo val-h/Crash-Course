f3d202e65d708a69a4ee058cb53e172e7ec2c384
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-05-27 14:50:04 +0100

    data visualization start

---
+++ Data visualization/Chapter 1/mpl_squares.py
@@ -0,0 +1,11 @@
+import matplotlib.pylot as plt
+
+squares = [1, 4, 9, 16 , 25]
+
+fig, ax = plt.subplot()
+ax.plot(squares)
+
+plt.show()
+
+# python package not found in PATH again....
+# fix for later 
