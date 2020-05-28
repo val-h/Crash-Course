ade79b2c78fb8d2975be127336c86d8303fad559
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-05-28 14:59:21 +0100

    fixed the typo

--- Data visualization/Chapter 1/mpl_squares.py
+++ Data visualization/Chapter 1/mpl_squares.py
@@ -1,11 +1,13 @@
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16 , 25]
 
-fig, ax = plt.subplot()
+fig, ax = plt.subplots()
 ax.plot(squares)
 
 plt.show()
 
 # python package not found in PATH again....
 # fix for later 
+
+# nvm that ... just 2 typos.......
