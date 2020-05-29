f2d102f5a8c1f81a39ca8b425c2d2d793c886864
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-05-29 22:10:41 +0100

    Better looking graph!

--- Data visualization/Chapter 1/mpl_squares.py
+++ Data visualization/Chapter 1/mpl_squares.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16 , 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Set the chart title and axes
+ax.set_title('Square numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
-
-# python package not found in PATH again....
-# fix for later 
-
-# nvm that ... just 2 typos.......
