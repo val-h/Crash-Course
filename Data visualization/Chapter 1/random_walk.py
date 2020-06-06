7744de64a2078a387e430bb0adcec45fb3bac01b
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-06-06 23:59:52 +0100

    Random walk generator

---
+++ Data visualization/Chapter 1/random_walk.py
@@ -0,0 +1,41 @@
+from random import choice
+
+class RandomWalk:
+    """A class to generate random walks"""
+
+    def __init__(self, num_points):
+        """
+        Attributes required for the walk: 'steps' variable for the count
+        and 2 lists to store the x, y coordinates of each step
+        """
+        self.num_points = num_points
+
+        # All walks start at (0, 0)
+        self.x = [0]
+        self.y = [0]
+    
+    def fill_walk(self):
+        """Calculate all the points in the walk"""
+        # Set the maximum movement
+        while len(self.x) < self.num_points:
+            # x steps calculation
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            # y steps calculation
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # reject steps that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # new position
+            x = self.x[-1] + x_step
+            y = self.y[-1] + y_step
+
+            # add the steps to their lists
+            self.x.append(x)
+            self.y.append(y)
---
+++ Data visualization/Chapter 1/rw_visual.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt
+
+from random_walk import RandomWalk
+
+# Create a random walk
+
+def visualize_walk(walk_steps):
+    """Vizualize the walk"""
+
+    # Generate the random walk pattern
+    rw = RandomWalk(walk_steps)
+    rw.fill_walk()
+
+    # creating the figure and scattering the steps on the plot
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    
+    ordered_steps = range(walk_steps)
+    ax.scatter(rw.x, rw.y, s=5, c=ordered_steps, cmap=plt.cm.Blues, edgecolors='none', label='Steps(lighter - beginning, darker-end')
+    
+    #Emphasize the first and last point
+    ax.scatter(0, 0, c='green', s=50, edgecolors='none', label='Start')
+    ax.scatter(rw.x[-1], rw.y[-1], c='red', s=50, edgecolors='none', label='Finish')
+
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+    ax.set_title('Random Walk Generator', fontsize=26)
+    
+    plt.legend()
+    plt.show()
+
+# Loop for the program to manipulate the walk and restart it
+while True:
+    print(' - Random Walk Generator - ')
+    generate_walk = input('Do you want to plot a pattern( y/n )? - ')
+    if generate_walk == 'y':
+        try:
+            walk_steps = int(input('How many steps to take? - '))            
+        except:
+            print('Invalid input! Please enter a valid integer.')
+            continue
+        else:
+            print(f'Generating a walk with {walk_steps} steps...')
+        
+        visualize_walk(walk_steps)
+    elif generate_walk == 'n':
+        print('Quiting program...')
+        break
