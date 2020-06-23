fdb8ef732a28510641b064e986bdb25b91b54629
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-06-23 14:14:56 +0100

    Added a visualization for the users

--- Data visualization/Chapter 3/hn_submissions.py
+++ Data visualization/Chapter 3/hn_submissions.py
@@ -7,7 +7,7 @@ import requests
 # Search the top threads
 subm_url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(subm_url)
-print('Status code:', r.status_code)
+print('Top stories request - Status code:', r.status_code)
 
 # Going through all of the submissions
 submission_ids = r.json()
@@ -17,7 +17,7 @@ for submission_id in submission_ids[:30]:
     # Make a seperate call for each submission
     url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
     sub_r = requests.get(url)
-    print(f'id: {submission_id}\tstatus: {sub_r.status_code}')
+    print(f'id: {submission_id}\t\tstatus: {sub_r.status_code}')
     response_dict = sub_r.json()
 
     # Build submission dictionary
@@ -33,7 +33,8 @@ for submission_id in submission_ids[:30]:
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
 
 # Creating the lists for the visualization and printing info about each discussion
-comments, names, labels = [], [], []
+print('\nStories')
+comments, names, labels, owners = [], [], [], []
 for submission_dict in submission_dicts:
     print(f'\nTitle - {submission_dict["title"]}')
     print(f'Discussion link - {submission_dict["hn_link"]}')
@@ -43,38 +44,94 @@ for submission_dict in submission_dicts:
     names.append(f'<a href="{submission_dict["hn_link"]}">{submission_dict["title"]}</a>')
     comments.append(submission_dict['comments'])
 
-    # Create a second visualization based on the users gathered here and their karma
     pt_1 = f'Owner: {submission_dict["owner"]}<br />'
     pt_2 = f'Score: {submission_dict["score"]}<br />'
     labels.append(f'{pt_1}{pt_2}')
 
-# Visualization
+    # List for the user profile visualization
+    owners.append(submission_dict['owner'])
 
-data = {
-    'type': 'bar',
-    'x': names,
-    'y': comments,
-    'hovertext': labels,
-    'marker': {
-        'color': 'orange',
-        'line': {'width': 2.5, 'color': 'green'},
-    },
-    'opacity': 0.6,
-}
+# HN users
+print('\nUsers')
+users, user_ids, user_karmas, user_labels = [], [], [], []
+for user_id in owners:
+    url = f'https://hacker-news.firebaseio.com/v0/user/{user_id}.json'
+    r = requests.get(url)
+    print(f'ID: {user_id}\t\t Status: {r.status_code}')
+    user_info = r.json()
 
-cst_layout = {
-    'title': 'Hacker News Top 30 Discussions',
-    'xaxis': {
-        'title': 'Names',
-        'titlefont': {'size': 26},
-        'tickfont': {'size': 14},
-    },
-    'yaxis': {
-        'title': 'Comments',
-        'titlefont': {'size': 26},
-        'tickfont': {'size': 14},
+    user_dict = {
+        'id': f'<a href="https://news.ycombinator.com/user?id={user_id}">{user_id}</a>',
+        'karma': user_info['karma'],
     }
-}
 
-figure = {'data': data, 'layout': cst_layout}
-offline.plot(figure, filename='hn_top_30.html')
+    user_ids.append(user_dict['id'])
+    user_karmas.append(user_dict['karma'])
+
+# Visualizations
+def TopStoriesVisualization():
+    data = {
+        'type': 'bar',
+        'x': names,
+        'y': comments,
+        'hovertext': labels,
+        'marker': {
+            'color': 'orange',
+            'line': {'width': 2.5, 'color': 'green'},
+        },
+        'opacity': 0.6,
+    }
+
+    cst_layout = {
+        'title': 'Hacker News Top 30 Discussions',
+        'xaxis': {
+            'title': 'Names',
+            'titlefont': {'size': 26},
+            'tickfont': {'size': 14},
+        },
+        'yaxis': {
+            'title': 'Comments',
+            'titlefont': {'size': 26},
+            'tickfont': {'size': 14},
+        }
+    }
+
+    figure = {'data': data, 'layout': cst_layout}
+    offline.plot(figure, filename='hn_top_30.html')
+
+def UsersKarmaVisualization():
+    data = {
+        'type': 'bar',
+        'x': user_ids,
+        'y': user_karmas,
+        'marker': {
+            'color': 'green',
+            'line': {'width': 2.5, 'color': 'yellow'},
+        },
+        'opacity': 0.7,
+    }
+
+    cst_layout = {
+        'title': 'Hacker News, profiles of the authors of the top 30 stories atm',
+        'xaxis': {
+            'title': 'IDs',
+            'titlefont': {'size': 26},
+            'tickfont': {'size': 14},
+        },
+        'yaxis': {
+            'title': 'Karma',
+            'titlefont': {'size': 26},
+            'tickfont': {'size': 14},
+        },
+    }
+
+    figure = {'data': data, 'layout': cst_layout}
+
+    offline.plot(figure, filename='hm_top_30_users.html')
+
+def main():
+    UsersKarmaVisualization()
+    #TopStoriesVisualization()
+
+if __name__ == '__main__':
+    main()
